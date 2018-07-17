8ddd7bca851b7a2d0bf3aa82c8f8a86c07a83faa
ryeon <ryeonnaderi@gmail.com>
2018-07-16 21:39:34 -0400

    changed some stuff

--- liri.js
+++ liri.js
@@ -1,1 +1,9 @@
 require("dotenv").config();
+client.post("status/update",{status:"I lose Twitter"},function(error,tweet,response){
+    if(error)
+    {
+    return(error);
+    }
+    console.log(tweet);
+    console.log(response);
+});
